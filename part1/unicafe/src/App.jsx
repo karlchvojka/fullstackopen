3d61a729b1d7a4a4b51e8fcfa2ef7b4105d16c8c
karlchvojka <karl.chvojka@gmail.com>
2025-09-15 15:03:03 -0400

    Part1: 1.7 unicafe step 2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -7,14 +7,43 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  // Helper Functions
+  const handleGoodClick = () => {
+    const uptGood = good + 1
+    setGood(uptGood)
+  }
+
+  const handleNeutClick = () => {
+    const uptNeut = neutral + 1
+    setNeutral(uptNeut)
+  }
+
+  const handleBadClick = () => {
+    const uptBad = bad + 1
+    setBad(uptBad)
+  }
+
+  const calcAll = good + bad + neutral
+
+  const calcAvg = (good * 1 + neutral * 0 + bad * (-1))/calcAll
+
+  const calcPos = parseFloat(good / calcAll) * 100
+
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <button onClick={handleGoodClick}>good</button>
+      <button onClick={handleNeutClick}>neutral</button>
+      <button onClick={handleBadClick}>bad</button>
       <h2>statistics</h2>
-      <p>good {good} <br/> neutral {neutral} <br/> bad {bad}</p>
+      <p>
+        good {good}<br/>
+        neutral {neutral}<br/>
+        bad {bad}<br/>
+        all {calcAll}<br/>
+        average {calcAvg}<br/>
+        positive {calcPos} %
+      </p>
     </div>
   )
 }
