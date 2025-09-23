e2b35949de6b26df9f9449454c7b0639bdd63e0a
karlchvojka <karl.chvojka@gmail.com>
2025-09-23 12:45:55 -0400

    Part2: 2.2: Course information step 7

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,7 +1,10 @@
 import Header from './Header'
 import Part from './Part'
+import Total from './Total'
 
 const Course = ({ course }) => {
+
+    const total = course.parts.reduce((acc, curr) => acc + curr.exercises, 0)
   return (
     <>
       <Header title={course.name} />
@@ -9,6 +12,7 @@ const Course = ({ course }) => {
         course.parts.map(part => 
           <p key={part.id}>{part.name} {part.exercises}</p>
         )}
+      <Total total={total} />
     </>
   )
 }
