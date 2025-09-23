c39d752ee2bb2a13e6d4f7613baa4753ed135c04
karlchvojka <karl.chvojka@gmail.com>
2025-09-23 11:19:25 -0400

    INIT: 2.1: Course Information step 6

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,57 @@
+const Content = (props) => {
+  return (
+    <>
+      <Part partName={props.parts[0].name} partExer={props.parts[0].exercises} />
+      <Part partName={props.parts[1].name} partExer={props.parts[1].exercises} />
+      <Part partName={props.parts[2].name} partExer={props.parts[1].exercises} />
+    </>
+  )
+}
+
+const Header = (props) => {
+  return (
+    <h1>{props.title}</h1>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>{props.partName} {props.partExer}</p>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header title={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default App
