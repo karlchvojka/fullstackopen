fe9c486dee284f91c067a2dde1c24d4ab30bcd7d
karlchvojka <karl.chvojka@gmail.com>
2025-09-24 15:50:46 -0400

    Part2: 2.11: The Phonebook Step 6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,8 @@
+// Library Imports
+import axios from 'axios'
+
 // Framework imports
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 // Component imports
 import Filter from './components/Filter'
@@ -8,18 +11,22 @@ import PersonForm from './components/PersonForm'
 
 const App = () => {
   // State Declarations
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]);
+  const [persons, setPersons] = useState([]);
   const [filtered, setFiltered] = useState([]);
   const [newValue, setNewValue] = useState({ name: '', number: ''});
   const [searchTerm, setSearchTerm] = useState('');
 
+  // On first render
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
   // Helper Functions
-  
   const cleaned = (str) => { return str.split(' ').join('') };
   const cleanedNames = persons.map(person => {return cleaned(person.number)})
 
